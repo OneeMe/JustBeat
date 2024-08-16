8e3a11cc989bae0ed0de0400fcf5ccca6e304c61
Lukathis <xuchi16@126.com>
2024-08-16 16:22:33 +0800

    feat: remove over complicated scroll view

--- JustBeat/Views/AlbumView.swift
+++ JustBeat/Views/AlbumView.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 
 struct AlbumView: View {
-    @State private var scrollEffectValue: Double = 13
-    @State private var activePageIndex: Int = 0
+    
     @StateObject var gameManager: GameManager = GameManager.shared
     
     let tileWidth: CGFloat = 220
     let tilePadding: CGFloat = 20
     
     var body: some View {
-        PagingScrollView(activePageIndex: self.$activePageIndex, tileWidth:self.tileWidth, tilePadding: self.tilePadding){
-            ForEach(0 ..< songs.count, id: \.self) { index in
-                GeometryReader { geometry2 in
-                    TileView(name: songs[index].songName)
-                        .rotation3DEffect(Angle(degrees: Double((geometry2.frame(in: .global).minX - self.tileWidth*0.5) / -10 )), axis: (x: 0, y: 1, z: 0))
-                        .onTapGesture {
-                            print ("tap on index: \(index)")
-                            self.gameManager.selectedSong = songs[index]
-                            self.gameManager.gameScene = songs[index].defaultGameScene
-                        }
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20) {
+                ForEach(0 ..< songs.count, id: \.self) { index in
+                    VStack {
+                        RoundedRectangle(cornerRadius: 20).fill(Color.gray)
+                            .overlay(GeometryReader { geometry in
+                                Image(songs[index].songName)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geometry.size.width, height: geometry.size.height)
+                                    .cornerRadius(20)
+                            })
+                            .frame(width: 250, height: 250)
+                        
+                        Text(songs[index].songName)
+                            .font(.title)
+                    }
+                    .onTapGesture {
+                        print ("tap on index: \(index)")
+                        self.gameManager.selectedSong = songs[index]
+                        self.gameManager.gameScene = songs[index].defaultGameScene
+                    }
+                    .padding()
+                    .glassBackgroundEffect()
                 }
             }
         }
